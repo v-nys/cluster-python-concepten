75e4e1bc6f23d06b8a039cc1c3410e1e8850b7ee
Vincent Nys <vincentnys@gmail.com>
2025-01-12 20:50:09 +0100

    Found a way to share schema across original and generated contents file.

--- flake.nix
+++ flake.nix
@@ -11,8 +11,11 @@
       in
       {
         devShell = pkgs.mkShell {
+          # setting env variable "schema"
+          # means both pre-contents.lc.yaml and contents.lc.yaml can use same line to inform LSP
           shellHook =
             ''
+              export schema="\$schema";
               if [ -z ''${LBLP_SOURCE_DIR} ]; then
                 echo "The environment variable LBLP_SOURCE_DIR must be defined. Set it to the directory containing the LBLP source code."
                 exit 1
